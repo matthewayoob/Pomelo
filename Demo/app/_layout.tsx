db012cb61d91098422a8e571b9fd81c4a1e4545c
ethlaude <149339033+ethlaude@users.noreply.github.com>
2024-11-13 22:46:56 -0800

    edits

--- Demo/app/_layout.tsx
+++ Demo/app/_layout.tsx
@@ -1,5 +1,20 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="next"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
 }
